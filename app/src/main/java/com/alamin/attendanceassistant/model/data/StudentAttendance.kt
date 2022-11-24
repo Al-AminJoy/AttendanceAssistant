ceb56f65a885a4bad8b1c386e7a74ed75ea30eac
Al-AminJoy <alaminislam3555@gmail.com>
2022-11-24 18:39:33 +0600

    Student Attendance Layout Added

---
+++ app/src/main/java/com/alamin/attendanceassistant/model/data/StudentAttendance.kt
@@ -0,0 +1,10 @@
+package com.alamin.attendanceassistant.model.data
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class StudentAttendance(
+val studentId:Int,
+val studentName:String,
+val isPresent: Boolean = false): Parcelable

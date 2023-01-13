ddbd148fbb46c232295aa1aa877b0635f0d055f7
Al-AminJoy <alaminislam3555@gmail.com>
2023-01-13 20:44:44 +0600

    Splash Screen & Logo Added

--- app/src/main/java/com/alamin/attendanceassistant/view/activity/SplashActivity.kt
+++ app/src/main/java/com/alamin/attendanceassistant/view/activity/SplashActivity.kt
@@ -1,14 +1,22 @@
 package com.alamin.attendanceassistant.view.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 import com.alamin.attendanceassistant.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        lifecycleScope.launchWhenCreated {
+            delay(1000)
+            startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+            finish()
+        }
     }
 }
